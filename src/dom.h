542124ddd30c0e86ee3cb9ad8ee0ef0c2750bb6b
smiya10 <sydsaki@gmail.com>
2016-12-27 23:47:42 -0500

    First full specification of interface

--- src/dom.h
+++ src/dom.h
@@ -1,10 +1,13 @@
+#ifndef DOM_H
+#define DOM_H
+
 #include <vector>
 #include <string>
 #include <unordered_map>
 
 // Type definitions
 // Represents the attributes of an element as a hash map between name and value
-typedef att_map unordered_map<string, string>;
+typedef std::unordered_map<std::string, std::string> att_map;
 
 
 // Represents the DOM data structure
@@ -17,7 +20,7 @@ class node
 
 		// Accessors
 		virtual bool has_children() const;
-		virtual int node_type() const;
+		virtual int get_node_type() const;
 		virtual std::string get_name() const;
 		virtual std::string get_value() const;
 		virtual std::string get_content() const;
@@ -37,35 +40,48 @@ class node
 		node last_child;
 		std::string node_value;
 		std::string text_content;
-}
+};
 
 // Element Class
 
-// Inheritance used, per the DOM specification
 class element: public node
 {
 	public:
 		// Constructors
-		element(string name, att_map attributes, vector<node> children);
-
-		// Accessors        
+		element(std::string name, att_map attributes, 
+		            std::vector<node> children);
+        element(std::string name, att_map attributes);
+        element(std::string name);
+        
+		// Accessors
+		bool has_attributes() const;
+		bool has_attribute(std::string name) const;        
 		att_map get_attributes() const;
+		std::vector<std::string> get_attribute_names() const;
 
 		// Mutators
-
+        void set_attribute(std::string name);
+        void remove_attribute(std::string name);
+        
 	private:
 		att_map attributes;
 		
-}
+};
 
 // Text class 
 
 class text: public node
 {
 	public:
-		text(string value);
+	    // Constructors
+		text(std::string value);
+		
 		// Accessor methods
+		std::string get_text();
 		
 	protected:
-		string value;
-}
+		std::string value;
+};
+
+
+#endif
